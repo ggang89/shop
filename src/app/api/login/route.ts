8b7003674b2aba842186629f04a26fa812971f46
ggang89 <ggang89@gmail.com>
2025-05-20 19:23:11 +0900

    jwt => iron-session으로 교체

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,9 +1,19 @@
 import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "../../../lib/script";
-import jwt from "jsonwebtoken";
-import { serialize } from "cookie";
+import { getIronSession, IronSessionData } from "iron-session";
+//import { serialize } from "cookie";
+import { cookies } from "next/headers";
+import { sessionOptions } from "@/lib/session";
 
-export async function POST(req: NextRequest) {
+export async function GET(req: NextRequest) {
+  const session = await getIronSession(await cookies(), sessionOptions);
+  return NextResponse.json({
+    isOK: true,
+    session,
+  });
+}
+
+export async function POST(req: NextRequest, res: NextResponse) {
   const { email, password } = await req.json();
 
   const user = await prisma.user.findUnique({ where: { email } });
@@ -22,27 +32,38 @@ export async function POST(req: NextRequest) {
     );
   }
 
-  const token = jwt.sign(
-    { id: user.id, email: user.email },
-    process.env.JWT_SECRET!,
-    { expiresIn: "1h" }
+  // const token = jwt.sign(
+  //   { id: user.id, email: user.email },
+  //   process.env.JWT_SECRET!,
+  //   { expiresIn: "1h" }
+  // );
+
+  const session = await getIronSession<IronSessionData>(
+    await cookies(),
+    sessionOptions
   );
+
+  // session.user = {
+  //   id: user.id,
+  //   email: user.email,
+  // };
+
+  await session.save();
 
   const response = NextResponse.json({ isOK: true, message: "로그인 성공" });
 
   //https://developer.mozilla.org/ko/docs/Web/API/Headers
   //https://ko.javascript.info/cookie
-  response.headers.set(
-    "Set-Cookie",
-    serialize("token", token, {
-      httpOnly: true,
-      path: "/",
-      maxAge: 60 * 60,
-      sameSite: "lax",
-      secure: process.env.NODE_ENV === "production",
-    })
-  );
-  console.log("현재 NODE_ENV:", process.env.NODE_ENV);
+  // response.headers.set(
+  //   "Set-Cookie",
+  //   serialize("token", token, {
+  //     httpOnly: true,
+  //     path: "/",
+  //     maxAge: 60 * 60,
+  //     sameSite: "lax",
+  //     secure: process.env.NODE_ENV === "production", // false => http로 설정
+  //   })
+  // );
 
   return response;
 }
---
+++ src/lib/session.ts
@@ -0,0 +1,18 @@
+import { SessionOptions } from "iron-session";
+
+export const sessionOptions: SessionOptions = {
+  password: process.env.IRON_SESSION_SECRET as string,
+  cookieName: "iron-session",
+  cookieOptions: {
+    secure: process.env.NODE_ENV === "production",
+  },
+};
+
+declare module "iron-session" {
+  interface IronSessionData {
+    user?: {
+      id: number;
+      email: string;
+    };
+  }
+}
