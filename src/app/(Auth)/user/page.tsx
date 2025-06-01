5f79d536ffc124a453de9af290e27c35ee764a07
ggang89 <ggang89@gmail.com>
2025-06-02 01:35:42 +0900

    user page => axios get 요청으로 session 정보 가져오는 걸로 수정 /클라이언트 컴포넌트로 변경해야 해서 prisma도 삭제

--- src/app/(Auth)/user/page.tsx
+++ src/app/(Auth)/user/page.tsx
@@ -1,40 +1,52 @@
+'use client';
+
 import { Button } from "@/components/ui/button";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { SquarePen } from "lucide-react";
 import { Mail } from "lucide-react";
-import { prisma } from "@/lib/script";
-import { getIronSession } from "iron-session";
-import { cookies } from "next/headers";
-import { sessionOptions } from "@/lib/session";
+
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 
 type SessionUser = {
+  name: string;
   id: number;
   email: string;
   isLoggedIn: boolean;
 };
 
-export default async function User() {
-  const userInfo = await getIronSession<SessionUser>(
-    await cookies(),
-    sessionOptions
-  );
-  const { email: userEmail } = userInfo;
+export default function User() {
 
-  const user = await prisma.user.findUnique({
-    where: {
-      email: userEmail,
-    },
-  });
-  //console.log("user22", user);
+  const [user, setUser] = useState<SessionUser|null>(null);
 
+  useEffect(() => {
+    const getUser = async () => {
+      try {
+        const res = await axios.get("/api/login");
+        if (res.data.isOK) {
+          setUser(res.data.session);
+        }
+        console.log("res", res);
+      } catch (error) {
+        console.log("Error:", error);
+      }
+    };
+    getUser();
+  }, [setUser]);
+
+  console.log("user", user);
+  //{id: 4, email: 'abc@abc.com', isLoggedIn: true}
+ const writingBorad = () => {
+  alert("게시판 글쓰기 탭으로 이동합니다."); 
+  }
+  
   return (
     <div className="flex-wrap ">
       {/* 프로필 => id / 게시판 글쓰기 탭 */}
-      <section
-        className="border-2 p-4 m-4 rounded-lg 
-      "
-      >
+      <section className=" border-2 p-4 m-4 rounded-lg ">
         <h1 className="text-2xl font-bold text-center">내 정보</h1>
+
         <div className="mt-4 flex items-center justify-center gap-4">
           <Avatar>
             <AvatarImage src="https://github.com/shadcn.png" />
@@ -44,10 +56,9 @@ export default async function User() {
           <p className="flex items-center gap-1">
             {" "}
             <Mail /> {user?.email}
-          </p>
+          </p>{" "}
         </div>
-
-        <Button>
+        <Button className="w-[200px] m-auto mt-5 flex justify-center bg-gray-500 hover:bg-gray-600" onClick={writingBorad}>
           게시판 글쓰기 <SquarePen />
         </Button>
       </section>
